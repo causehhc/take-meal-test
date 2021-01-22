65c005e5d06e645abd398fe1c22b3f9f5ca418ce
causehhc <1005694755@qq.com>
2021-01-22 20:24:14 +0800

    test1.0

--- handle.py
+++ handle.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
 # filename: handle.py
 
 import hashlib
+import reply    # 导入回复文件
+import receive  # 导入接收文件
 import web
 
 
@@ -10,17 +11,19 @@ class Handle(object):
         try:
             data = web.input()
             if len(data) == 0:
-                return "hello, this is handle view"
+                return "hello world"
             signature = data.signature
             timestamp = data.timestamp
             nonce = data.nonce
             echostr = data.echostr
-            token = "Hhc142427"  # 请按照公众平台官网\基本配置中信息填写
+            token = 'hhc404'  # 请按照公众平台官网\基本配置中信息填写
 
             list = [token, timestamp, nonce]
             list.sort()
             sha1 = hashlib.sha1()
-            map(sha1.update, list)
+            sha1.update(list[0].encode('utf-8'))
+            sha1.update(list[1].encode('utf-8'))
+            sha1.update(list[2].encode('utf-8'))
             hashcode = sha1.hexdigest()
             print("handle/GET func: hashcode, signature: ", hashcode, signature)
             if hashcode == signature:
@@ -29,3 +32,28 @@ class Handle(object):
                 return ""
         except Exception as Argument:
             return Argument
+
+    def POST(self):    # 新增加的POST函数
+        try:
+            webData = web.data()
+            print("Handle Post webdata is ", webData)
+            # 后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg):
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                if recMsg.MsgType == 'text':
+                    content = "sb"  # 这里是回复内容
+                    replyMsg = reply.TextMsg(toUser, fromUser, content)
+                    return replyMsg.send()
+                if recMsg.MsgType == 'image':
+                    mediaId = recMsg.MediaId
+                    replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
+                    return replyMsg.send()
+                else:
+                    return reply.Msg().send()
+            else:
+                print("暂且不处理")
+                return reply.Msg().send()
+        except Exception as Argment:
+            return Argment
